bc8f41d54598b64620de9dd6345b3827517070da
Wolfgang Sanyer <wolfgang.sanyer@gmail.com>
2014-05-22 17:17:55 -0400

    Adding some data and code for Coursera & EdX courses

---
+++ code/functions.R
@@ -0,0 +1,51 @@
+checkWD <- function() {
+    if (!(getwd() == "/Users/wsanyer/projects/statistics")) {
+        setwd("/Users/wsanyer/projects/statistics")
+    }
+    
+    getwd()
+}
+
+downloadFileCSV <- function() {
+    dateDownloaded <- date()
+    fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+    destFile <- "./data/cameras.csv"
+    method <- "curl"
+    
+    download.file(fileUrl, destFile, method)
+}
+
+downloadFileXLSX <- function() {
+    fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+    destFile <- "./data/cameras.xlsx"
+    method <- "curl"
+    
+    download.file(fileUrl, destFile, method)
+    dateDownloaded <- date()
+}
+
+downloadJiraFilterXLSX <- function(fileUrl = "https://jira.benefitfocus.com/sr/jira.issueviews:searchrequest-excel-current-fields/24033/SearchRequest-24033.xls?tempMax=1000&os_username=wsanyer&os_password=fox11you", rootDir = "./data/jira/", fileName = "jira1.xls") {
+    if(!file.exists(rootDir)) { dir.create(rootDir) }
+    destFile <- paste(rootDir, fileName, sep = "")
+    method <- "curl"
+    
+    download.file(fileUrl, destFile, method)
+    dateDownloaded <- date()
+}
+
+downloadFileXML <- function(fileUrl="http://www.w3schools.com/xml/simple.xml") {
+    doc <- xmlTreeParse(fileUrl, useInternal = TRUE)
+    rootNode <- xmlRoot(doc)
+    xmlName(rootNode)
+}
+
+columnmean <- function(y, removeNA = TRUE) {
+    nc <- ncol(y)
+    means <- numeric(nc)
+    
+    for (i in 1:nc) {
+        means[i] <- mean(y[, i], na.rm = removeNA)
+    }
+    
+    means
+}
